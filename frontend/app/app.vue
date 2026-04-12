f7be7992087a6fb0e17ea0c1230a2aa5881f7946
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2026-04-12 11:51:38 -0500

    update background

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex max-w-5xl p-5 m-auto items-center justify-center min-h-screen">
+  <div class="flex max-w-5xl p-5 m-auto items-center justify-center min-h-screen bg-gray-100">
     <NuxtPage />
   </div>
 </template>
